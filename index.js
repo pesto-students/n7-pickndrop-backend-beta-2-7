ee2dc16518a7748ce1f9fb73ca4d5cdd46519088
Piyush Sharma <piyush.sharma.3101@gmail.com>
2021-09-11 02:42:09 +0530

    cors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import users from './services/users.js'
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import cors from 'cors';
 const url="mongodb+srv://dbUser:dbUserPassword@cluster0.xdwsy.mongodb.net/dbName?retryWrites=true&w=majority";
 const startServer=async ()=>{
     try{
         await mongoose.connect(url);
         const app=express();
         app.use(bodyParser.json());
+        app.use(cors());
         users(app);
         app.listen(4000);
     }catch(e){

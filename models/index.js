c196f80d6c0c6a2bc68698baeddc481cc3d4672f
Akash Kumar <akashkumar@Akashs-MacBook-Pro.local>
2021-09-12 20:36:58 +0530

    added delivery partner register schema

--- models/index.js
+++ models/index.js
@@ -1,9 +1,29 @@
-import mongoose  from "mongoose";
-const {model,Schema}=mongoose;
-const UserSchema=new Schema({
-    email:String,
-    phone:String,
-    role:String,
-    otp:String
+import mongoose from "mongoose";
+const { model, Schema } = mongoose;
+
+const UserSchema = new Schema({
+	email: String,
+	phone: String,
+	role: String,
+	otp: String,
 });
-export const User=model("User",UserSchema)
+
+const DriverSchema = new Schema({
+    firstName: String,
+	lastName: String,
+	fatherName: String,
+	city: String,
+	completeAddress: String,
+	language: String,
+	date: Date,
+	emergencyContact: Number,
+	workExperience: Number,
+	vehicleDetails: String,
+	panCard: String,
+	aadharCard: Number,
+	drivingLicense: String
+})
+
+export const User = model("User", UserSchema);
+
+export const Driver = model("Driver", DriverSchema);
--- services/users.js
+++ services/users.js
@@ -1,102 +1,134 @@
-import {User} from '../models/index.js'
-import {send} from '../utils/email.js';
-import {STATUS_OK,SERVER_ERROR} from '../constants/index.js';
-export default app=>{
-    app.post("/users/authenticate",async (req,res)=>
-    {
-        const {email,phone}=req.body;
-        const role="user";
-        const otp=Math.floor(Math.random()*10E5);
-        try{
-            const user=await User.findOne({
-                email,phone,role
-            });
-            if(!user){
-                const user=new User({
-                    email,
-                    phone,
-                    role
-                });
-                await user.save();
-            }
-            await User.updateOne({
-                email,
-                phone,
-                role
-            },{
-                otp
-            });
-            await send(email,`Your OTP is ${otp}`);
-            res.status(STATUS_OK);
-            return res.json({
-                email,
-                phone,
-                role,
-            });
-        }catch(e){
-            console.log(e);
-            res.status(SERVER_ERROR);
-            return res.end(e.toString())
-        }
-    });
-    app.post("/driver/authenticate",async (req,res)=>
-    {
-        const {email,phone}=req.body;
-        const role="driver";
-        const otp=Math.floor(Math.random()*10E5);
-        try{
-            const user=await User.findOne({
-                email,phone,role
-            });
-            if(!user){
-                const user=new User({
-                    email,
-                    phone,
-                    role
-                });
-                await user.save();
-            }
-            await User.updateOne({
-                email,
-                phone,
-                role
-            },{
-                otp
-            });
-            await send(email,`Your OTP is ${otp}`);
-            res.status(STATUS_OK);
-            return res.json({
-                email,
-                phone,
-                role,
-            });
-        }catch(e){
-            console.log(e);
-            res.status(SERVER_ERROR);
-            return res.end(e.toString());
-        }
-    });
-    app.post("/otp/authenticate",async (req,res)=>{
-        const {otp,email,phone}=req.body;
-        try{
-            const user=await User.findOne({
-                email,
-                phone,
-                otp
-            });
-            if(!user){
-                throw "Wrong OTP";
-            }
-            res.status(STATUS_OK);
-            return res.json({
-                email:user.email,
-                phone:user.phone,
-                role:user.role,
-            });
-        }catch(e){
-            console.log(e);
-            res.status(SERVER_ERROR);
-            return res.end(e.toString());
-        }
-    })
-}
+import { User } from "../models/index.js";
+import { send } from "../utils/email.js";
+import { STATUS_OK, SERVER_ERROR } from "../constants/index.js";
+export default (app) => {
+	app.post("/users/authenticate", async (req, res) => {
+		const { email, phone } = req.body;
+		const role = "user";
+		const otp = Math.floor(Math.random() * 10e5);
+		try {
+			const user = await User.findOne({
+				email,
+				phone,
+				role,
+			});
+			if (!user) {
+				const user = new User({
+					email,
+					phone,
+					role,
+				});
+				await user.save();
+			}
+			await User.updateOne(
+				{
+					email,
+					phone,
+					role,
+				},
+				{
+					otp,
+				}
+			);
+			await send(email, `Your OTP is ${otp}`);
+			res.status(STATUS_OK);
+			return res.json({
+				email,
+				phone,
+				role,
+			});
+		} catch (e) {
+			console.log(e);
+			res.status(SERVER_ERROR);
+			return res.end(e.toString());
+		}
+	});
+	app.post("/driver/authenticate", async (req, res) => {
+		const { email, phone } = req.body;
+		const role = "driver";
+		const otp = Math.floor(Math.random() * 10e5);
+		try {
+			const user = await User.findOne({
+				email,
+				phone,
+				role,
+			});
+			if (!user) {
+				const user = new User({
+					email,
+					phone,
+					role,
+				});
+				await user.save();
+			}
+			await User.updateOne(
+				{
+					email,
+					phone,
+					role,
+				},
+				{
+					otp,
+				}
+			);
+			await send(email, `Your OTP is ${otp}`);
+			res.status(STATUS_OK);
+			return res.json({
+				email,
+				phone,
+				role,
+			});
+		} catch (e) {
+			console.log(e);
+			res.status(SERVER_ERROR);
+			return res.end(e.toString());
+		}
+	});
+	app.post("/user/otp/authenticate", async (req, res) => {
+		const { otp, email, phone } = req.body;
+		try {
+			const user = await User.findOne({
+				email,
+				phone,
+				otp,
+			});
+			if (!user) {
+				throw "Wrong OTP";
+			}
+			res.status(STATUS_OK);
+			return res.json({
+				email: user.email,
+				phone: user.phone,
+				role: user.role,
+			});
+		} catch (e) {
+			console.log(e);
+			res.status(SERVER_ERROR);
+			return res.end(e.toString());
+		}
+	});
+
+	app.post("/driver/otp/authenticate", async (req, res) => {
+		const { otp, email, phone } = req.body;
+		try {
+			const user = await User.findOne({
+				email,
+				phone,
+				otp,
+			});
+			if (!user) {
+				throw "Wrong OTP";
+			}
+			res.status(STATUS_OK);
+			return res.json({
+				email: user.email,
+				phone: user.phone,
+				role: user.role,
+			});
+		} catch (e) {
+			console.log(e);
+			res.status(SERVER_ERROR);
+			return res.end(e.toString());
+		}
+	});
+};

6eda28d0a70ddce8e4849a7cbd0f0645a7e1954b
Akash Kumar <akashkumar@Akashs-MacBook-Pro.local>
2021-09-25 22:09:45 +0530

    added sender name and receiver name

--- models/index.js
+++ models/index.js
@@ -2,45 +2,48 @@ import mongoose from "mongoose";
 const { model, Schema } = mongoose;
 
 const UserSchema = new Schema({
-  email: String,
-  phone: String,
-  role: String,
-  otp: String,
+	email: String,
+	phone: String,
+	role: String,
+	otp: String,
 });
 
 const DriverSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  fatherName: String,
-  city: String,
-  completeAddress: String,
-  language: String,
-  date: Date,
-  emergencyContact: Number,
-  workExperience: Number,
-  vehicleDetails: String,
-  panCard: String,
-  aadharCard: Number,
-  drivingLicense: String,
+	firstName: String,
+	lastName: String,
+	fatherName: String,
+	city: String,
+	completeAddress: String,
+	language: String,
+	date: Date,
+	emergencyContact: Number,
+	workExperience: Number,
+	vehicleDetails: String,
+	panCard: String,
+	aadharCard: Number,
+	drivingLicense: String,
 });
+
 const TaskSchema = new Schema({
-  sender: {
-    address: String,
-    phoneNo: String,
-    latitude: Number,
-    longitude: Number,
-  },
-  receiver: {
-    address: String,
-    phoneNo: String,
-    latitude: Number,
-    longitude: Number,
-  },
-  title: String,
-  description: String,
-  price: Number,
+	sender: {
+		address: String,
+		phoneNo: String,
+		latitude: Number,
+		longitude: Number,
+		name: String,
+	},
+	receiver: {
+		address: String,
+		phoneNo: String,
+		latitude: Number,
+		longitude: Number,
+		name: String,
+	},
+	title: String,
+	description: String,
+	price: Number,
 });
+
 export const Task = model("Task", TaskSchema);
 export const User = model("User", UserSchema);
-
 export const Driver = model("Driver", DriverSchema);
